17dc1567eb8d1bcd3973ae237d9b7012cad5a7f1
Max Ryan <maxryan12345@hotmail.com>
2015-11-03 19:46:28 +0000

    Added exercise

---
+++ 2.groovy
@@ -0,0 +1,24 @@
+print "Enter a number: "
+str = System.console().readLine()
+double d1 = Double.parseDouble(str)
+print "Enter another number: "
+str = System.console().readLine()
+double d2 = Double.parseDouble(str)
+print "Enter an operator: "
+String op = System.console().readLine()
+if (op == "+") {
+	result = d1 + d2
+	println result
+} else if (op == "-") {
+	result = d1 - d2
+	println result
+} else if (op == "*") {
+	result = d1 * d2
+	println result
+} else if (op == "/") {
+	result = d1 / d2
+	println result
+} else {
+	println "wrong"
+}
+System.console().readLine()
